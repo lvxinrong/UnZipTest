66c361f3c368bdfd73a0898090736cf3dc07c354
山荣火斤口口 <825883336@qq.com>
2017-05-26 17:57:41 +0800

    参数异常修改

--- src/com/lv/unZip/Main.java
+++ src/com/lv/unZip/Main.java
@@ -12,8 +12,11 @@ public class Main {
 			//压缩
 			String fileZipName = args[1];
 			String zipSource = args[2];
+			File file = new File(zipSource);
+			if(!file.exists()) 
+				throw new IllegalArgumentException("压缩源文件不存在");
 			try {
-				MyZipUtil.zip(fileZipName, new File(zipSource));
+				MyZipUtil.zip(fileZipName, file);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -21,6 +24,8 @@ public class Main {
 		}else if(method.equals("2")) {
 			String fileZipName = args[1];
 			String destName = args[2];
+			if(!new File(fileZipName).exists())
+				throw new IllegalArgumentException("解压源文件不存在");
 			try {
 				MyZipUtil.unZip(fileZipName, destName);
 			} catch (Exception e) {
